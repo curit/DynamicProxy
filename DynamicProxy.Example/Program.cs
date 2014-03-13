731503cfd1d89fd2f1f7d02c84e0fa8729cd8c6b
Albert-Jan Nijburg <albertjan@curit.com>
2014-03-13 10:00:34 +0100

    Adds a bit more example still.

--- DynamicProxy.Example/Program.cs
+++ DynamicProxy.Example/Program.cs
@@ -1,18 +1,27 @@
 ﻿namespace DynamicProxy.Example
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public interface IBook
     {
         int NumberOfPages { get; set; }
         int CurrentPage { get; set;  }
         void NextPage();
+        int[] AddChapter(string name, int startpage, int endpage);
     }
 
     public sealed class Book
     {
+        public Book()
+        {
+            Chapters = new List<Tuple<string, int[]>>();
+        }
+
         public int NumberOfPages { get; set; }
         public int CurrentPage { get; private set; }
+        private List<Tuple<string,int[]>> Chapters { get; set; } 
 
         public void NextPage()
         {
@@ -21,6 +30,12 @@
                 throw new EndOfBookException("You've reached the end");
             }
             CurrentPage++;
+        }
+
+        public int[] AddChapter(string name, int startpage, int endpage)
+        {
+            Chapters.Add(Tuple.Create(name, Enumerable.Range(startpage, endpage-startpage).ToArray()));
+            return Enumerable.Range(startpage, endpage - startpage).ToArray();
         }
     }
 
@@ -57,15 +72,31 @@
                 }
             });
 
+            //we want to include an extra page in the chapter 
+            proxy.AddTransformer<int>(c => c.AddChapter(A<string>.PlaceHolder, A<int>.Selected, A<int>.PlaceHolder), Direction.In, i => i - 1);
+
+            //we want to intercept it because we can!
+            proxy.AddInterceptor<string, int, int, int[]>((b, name, start, end) => b.AddChapter(name, start, end),
+                (func, s, i, i2) =>
+                {
+                    Console.Out.WriteLine("Start: " + i);
+                    Console.Out.WriteLine("End: " + i2);
+                    Console.Out.WriteLine("Adding chapter " + s + " containing " + (i2 - i) + " pages");
+                    return func(s, i, i2);
+                });
+            
             Console.Out.WriteLine("Number of pages: " + proxiedBook.NumberOfPages);
             Console.Out.WriteLine("Current page: " + proxiedBook.CurrentPage);
               
-            while (proxiedBook.CurrentPage <= proxiedBook.NumberOfPages)
+            while (proxiedBook.CurrentPage != proxiedBook.NumberOfPages)
             {
                 proxiedBook.NextPage();
                 Console.Out.Write(proxiedBook.CurrentPage + ", ");
             }
+            //No exception here!
+            Console.Out.WriteLine(proxiedBook.CurrentPage);
 
+            proxiedBook.AddChapter("1st Chapter", 6, 42);
             Console.In.ReadLine();
         }
     }
